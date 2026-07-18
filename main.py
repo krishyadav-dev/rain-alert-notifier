f1dda2b134796a5b1bb6bd8287bf7043ba69b580
Krish Yadav <krish180305@gmail.com>
2026-07-19 01:53:08 +0530

    Implement rain alert automation script
    Integrate OpenWeatherMap API to parse weather condition codes and Twilio API to dispatch SMS notifications. Utilize environment variables to securely handle API keys and authentication tokens.

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from twilio.rest import Client
+
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+weather_params = {
+    "lat" : 18.455898267850273,
+    "lon" : 73.84747593637942,
+    "appid" : api_key,
+    "cnt" : 4
+}
+response = requests.get(OWM_endpoint, weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code < 700:
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body = "It's going to rain today. Remember to keep an umbrella ☔, Love You",
+            from_ = '+13194036010',
+            to = '+918482800151'
+        )
+        print("Bring an umbrella!")
+        break
